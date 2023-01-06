e4526a9c9503e63978583b22277c803e7de69843
Uesleyrodi <uesley_rsantos2@hotmail.com>
2023-01-06 09:28:47 -0300

    Construindo arquivo Hooks

--- NinjaPixelWeb/Hooks.cs
+++ NinjaPixelWeb/Hooks.cs
@@ -1,6 +1,32 @@
-﻿namespace NinjaPixelWeb
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.Events;
+using System;
+
+namespace NinjaPixelWeb
 {
-    internal class Hooks
+    public class Hooks
     {
+        private static ChromeDriver ChromeDriver;
+        private static EventFiringWebDriver Driver;
+
+        [TestInitialize]
+        public void MyTestInitialize()
+        {
+            ChromeDriver = new ChromeDriver("Deploy");
+            ChromeDriver.Url = "";
+            ChromeDriver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(60);
+            ChromeDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(60);
+            ChromeDriver.Manage().Window.Maximize();
+
+            Driver = new EventFiringWebDriver(ChromeDriver);
+        }
+
+        [TestCleanup]
+        public void MyTestCleanup()
+        {
+            ChromeDriver.Quit();
+            Driver.Quit();
+        }
     }
 }
